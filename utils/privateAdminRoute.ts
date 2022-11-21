c703bf1381147de4afbb66033ef5f1e347a35f33
ivanrenedo <renedoivan@gmail.com>
2022-11-21 14:52:18 +0100

    cuarta subida

--- utils/privateAdminRoute.ts
+++ utils/privateAdminRoute.ts
@@ -1,7 +1,6 @@
 import cookie from "cookie";
 import jwtDecode from "jwt-decode";
 import { isBrowser } from "./isBrowser";
-import redirect from "./redirect";
 
 
 export const privateRouteAdmin = (context) => {
@@ -9,17 +8,17 @@ export const privateRouteAdmin = (context) => {
     const cookies = cookie.parse(context.req ? context.req.headers.cookie || "" : document.cookie);
 
     if (!isBrowser && cookies.rsht === undefined) {
-        redirect(context.res, '/backoffice/signin');
+        /* redirect(context.res, '/backoffice/signin'); */
     } 
 
     const {role}:any = jwtDecode(cookies.rsht)
      
     try{
         if (!isBrowser && role != 'ADMIN' && !isBrowser && role != 'SUPER_ADMIN') {
-            redirect(context.res, '/backoffice/signin')
+            /* redirect(context.res, '/backoffice/signin') */
         }
     }catch(err){
-        redirect(context.res, '/backoffice/signin');
+        /* redirect(context.res, '/backoffice/signin'); */
     }
     
 }
