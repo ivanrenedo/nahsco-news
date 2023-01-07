fc4758a2091b19b47677f139425a828d620e7fc5
ivanrenedo <renedoivan@gmail.com>
2023-01-07 20:20:57 +0100

    adsense

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -63,6 +63,7 @@ class MyDocument extends Document {
                     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
                     <meta name="msapplication-TileColor" content="#da532c" />
                     <meta name="theme-color" content="#ffffff"></meta>
+                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3145765907918273" crossOrigin="anonymous"></script>
                 </Head>
                 <body className="body-class" id="nahsco">
                     <script
@@ -75,7 +76,6 @@ class MyDocument extends Document {
                         async
                         src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
                     />
-                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2621121538375000" crossOrigin="anonymous"></script>
                     <script
                         dangerouslySetInnerHTML={{
                         __html: `
